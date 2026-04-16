c614f397fc8c57aad9d942dfa49d59c815be5616
platz-halter <simon.pressel@gmx.de>
2026-04-16 16:40:03 +0200

    Updated router

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.v1 import auth, categories, chores, users
+from app.api.v1 import auth, categories, chores, stats, takeover, users
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(auth.router)
 router.include_router(users.router)
 router.include_router(categories.router)
 router.include_router(chores.router)
+router.include_router(takeover.router)
+router.include_router(stats.router)
